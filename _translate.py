ffe6e95e1e8d60bab0280394f460b1a79ec0da7c
john jayson b. de leon <savjaylade84@gmail.com>
2021-04-07 02:28:52 +0800

    re-edit translate file

--- _translate.py
+++ _translate.py
@@ -1,16 +1,29 @@
 from translate import Translator
 
-print("<example: file.txt")
-filename=input("[Enter Filename]:")
+#     get the user prefer file and language to translate
 
-translator = Translator(to_lang="german")
+print("example: file.txt")
+filename=input(">>>[Enter Filename]:")
+
+print("example:german")
+language=input(">>>[Enter Language]:")
+
+#    create translate object and set prefer language to use to translate
+#    the original language 
+
+translator = Translator(to_lang=language)
+
+#     try to extract all the file text then print it
+#     to show user the original then translate the text
+#     to the prefer language then print the so that the user
+#     know that the translation works
 
 try:
     with open(filename,'r') as reader:
         for line in reader:
             line.strip()
-            print(line)
+            print(">>>[original]:{0}",line)
             translation=translator.translate(line)
-            print(translation)
+            print(">>>[translated]:{0}",translation)
 except:
    print("[File Not Exist Or Wrong Path File]")
